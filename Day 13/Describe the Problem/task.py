a1839d9bd69e17302b56fd4f7155ecdc6e087327
Kritika Jha <kritikajha2107@gmail.com>
2025-12-14 00:29:18 +0530

    Initial commit

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# taking range from 1 to 19 as 20 is not included in it
 # 2. When is the function meant to print "You got it"?
+
 # 3. What are your assumptions about the value of i?
+#  it comprises of values from 1 to 19
--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,9 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You did enter an invalid number. Kindly enter a numerical entry such as 23.")
+    age = int(input("How old are you?"))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at  {age}.")
+else:
+    print("Cannot drive  ")
